1398837d07ee923bbbf74d472de85ebee1a9209a
Sai Harshitha Akkina <120376983+harshitha-2201@users.noreply.github.com>
2024-08-15 18:21:42 +0530

    Update Register.jsx
    Added backend live server

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post('http://localhost:3001/api/register', formData);
+      await axios.post('https://task-manager-px9u.onrender.com', formData);
       alert('Registration successful!');
       navigate('/login');
     } catch (error) {
